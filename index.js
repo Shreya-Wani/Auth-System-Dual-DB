7a2c373ae0f561a2ed758f653cae7aecaa40f400
Shreya Wani <shreyawani2006@gmail.com>
2026-01-26 22:09:26 +0530

    Set up initial Express server and middleware

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config()
+
+const app = express();
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowheaders: ['Content-Type', 'Authorization']
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
